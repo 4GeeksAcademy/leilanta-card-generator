569531471a5e3a74fdeffa72d1da5837ba04495c
Leilani Tanimoto Abad <135250913+Leilanta@users.noreply.github.com>
2023-10-24 15:01:09 +0000

    ejercicio carta extra

--- src/app.js
+++ src/app.js
@@ -2,10 +2,35 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const suits = ["♥", "♠", "♣", "♦"];
+const cards = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  randomCard();
+  setInterval(window.onload, 5000);
+};
+
+const randomCard = function() {
+  let card = cards[Math.floor(Math.random() * cards.length)];
+  let suit = suits[Math.floor(Math.random() * suits.length)];
+  document.querySelector("#randomCard").innerHTML = card;
+  document.querySelector("#randomSuit").innerHTML = suit;
+  document.querySelector("#randomSuitFooter").innerHTML = suit;
+
+  if (suit === "♥" || suit === "♦") {
+    document.querySelector("#randomSuit").style.color = "red";
+    document.querySelector("#randomSuitFooter").style.color = "red";
+  } else {
+    document.querySelector("#randomSuit").style.color = "black";
+    document.querySelector("#randomSuitFooter").style.color = "black";
+  }
+};
+
+window.changeDimensions = function changeDimensions() {
+  let width = document.querySelector("#cardWidth").value;
+  let height = document.querySelector("#cardHeight").value;
+
+  document.querySelector(".card").style.width = width + "px";
+  document.querySelector(".card").style.height = height + "px";
 };
